5a55151e9b268646e4957fa9c6dd54f9a1810520
dcgutierrez93 <dcgutierrez93@yahoo.com>
2017-12-02 17:36:36 -0600

    adding

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+// Create a public class Calculator.
+public class Calculator {
+    // Create a Calculator constructor.
+    public Calculator() {
+
+    }
+
+    // Create a public method that returns int and call it add.
+    // Should take two parameters int a and int b
+    public void add(int a, int b) {
+        return a + b;
+        return int;
+    }
+
+    // Create a main method.
+    public static void main(String[] args) {
+        // Create an Calculator Object called myCalculator
+        Calculator myCalculator = new Calculator
+    }
+}
