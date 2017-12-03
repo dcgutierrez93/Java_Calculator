8068e9c7e713830daeb79f51a367b6b49aedd21a
David Gutierrez <dcgutierrez93@yahoo.com>
2017-12-03 12:50:55 -0600

    Working Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,46 @@
-// Create a public class Calculator.
+// Create a public class Calculator
 public class Calculator {
-    // Create a Calculator constructor.
+    // Create constructor.
     public Calculator() {
 
     }
-
-    // Create a public method that returns int and call it add.
-    // Should take two parameters int a and int b
-    public void add(int a, int b) {
+    // Create add method
+    public int add(int a, int b) {
         return a + b;
-        return int;
+    }
+    // Create subtract method.
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    // Create a multiply method.
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    // Create a divide method.
+    public int divide(int a, int b) {
+        // Check error do see if dividing by 0
+        if (b == 0) {
+            System.out.println("Error! Dividing by zero is not allowed.");
+            return 0;
+        } else {
+            return a / b;
+        }
+    }
+    // Create a modulo method.
+    public int modulo(int a, int b) {
+        // Check error to see if modulo by 0
+        if (b == 0) {
+            System.out.println("Error! Dividing by zero is not allowed.");
+            return 0;
+        } else {
+            return a % b;
+        }
     }
 
-    // Create a main method.
     public static void main(String[] args) {
-        // Create an Calculator Object called myCalculator
-        Calculator myCalculator = new Calculator
+        // Create a Calculator object called myCalculator
+        Calculator myCalculator = new Calculator();
+        System.out.println(myCalculator.add(5, 7));
+        System.out.println(myCalculator.subtract(45, 11));
     }
 }
